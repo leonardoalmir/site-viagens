b92d0e97d8d5371e9df9619c40bd37da8be2b1f8
Leonardo Almir <leonardostynner@live.com>
2024-02-07 21:58:01 -0300

    criando o slider de video e conteudo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,36 @@
+// Menu do header resposnivo
 const menuBtn = document.querySelector(".menu-mobile-btn");
 const navegacao = document.querySelector(".navegacao");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navegacao.classList.toggle("active");
+});
+
+// Menu de navegação de slides
+const slideBotoes = document.querySelectorAll(".nav-botao");
+const videos = document.querySelectorAll(".video-slide");
+const conteudos = document.querySelectorAll(".conteudo");
+
+var slideMenu = function (manual) {
+    slideBotoes.forEach((slideBotao) => {
+        slideBotao.classList.remove('active');
+    });
+    videos.forEach((video) => {
+        video.classList.remove('active');
+    });
+    conteudos.forEach((conteudo) => {
+        conteudo.classList.remove('active');
+    });
+
+    slideBotoes[manual].classList.add('active');
+    videos[manual].classList.add('active');
+    conteudos[manual].classList.add('active');
+
+};
+
+slideBotoes.forEach((slideBotao, i) => {
+    slideBotao.addEventListener("click", () => {
+        slideMenu(i);
+    });
 });
